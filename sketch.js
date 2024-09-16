c52f9ba25af644f2104206e48d77f30ff421dc1b
carloseduardogon <00001112056701sp@al.educacao.sp.gov.br>
2024-09-16 18:08:43 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,43 @@
+let posicaoHorizontal;
+let posicaoVertical;
+let cor;
+
+function setup() 
+{
+  createCanvas(400, 400);
+  background(220);
+  posicaoHorizontal=200;
+  posicaoVertical=200;
+  cor=color(random(0,255),random(0,255),random(0,255));
+}
+function draw() 
+{
+  circle(posicaoHorizontal,posicaoVertical,50);
+  fill(cor);
+  
+  if(mouseX<=posicaoHorizontal)
+    {
+      posicaoHorizontal=posicaoHorizontal-1;
+  }
+  
+  if(mouseX>=posicaoHorizontal)
+    {
+      posicaoHorizontal=posicaoHorizontal+1;
+  }
+  
+  if(mouseY<=posicaoVertical)
+    {
+      posicaoVertical=posicaoVertical-1;
+  }
+  
+  if(mouseY>=posicaoVertical)
+    {
+      posicaoVertical=posicaoVertical+1;
+  }
+  
+  if(mouseIsPressed)
+    {
+      cor=color(random(0,255),random(0,255),random(0,255));
+  }
+  
+}
